965f422f987b78011b64933ae74035cc3622548e
Osiris8 <miganosiris8@gmaaail.com>
2023-10-08 23:50:26 +0200

    start of the structure of App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"; // Vous pouvez créer un fichier CSS personnalisé pour ajouter des styles supplémentaires
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="bg-blue-500 min-h-screen flex flex-col justify-center items-center text-white">
+      {/* En-tête avec les liens */}
+      <div className="text-4xl font-bold mb-8">
+        <a href="#" className="mr-4">
+          Projet
         </a>
-      </header>
+        <a href="#">Contact</a>
+      </div>
+      {/* Votre nom au centre */}
+      <h1 className="text-6xl font-bold mb-4">Votre Nom</h1>
+      {/* Paragraphe de description */}
+      <p className="text-lg max-w-xl text-center">
+        Enthousiaste du développement web spécialisé dans React, Node.js et
+        MongoDB. Passionné par la création de solutions innovantes et élégantes.
+      </p>
     </div>
   );
 }
