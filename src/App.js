883fdad48a7a22d7962265dd83cfd0a965a923d7
Osiris8 <miganosiris8@gmaaail.com>
2023-10-09 11:38:09 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="bg-blue-500 min-h-screen flex flex-col justify-center items-center text-white">
       {/* En-tête avec les liens */}
-      <div className=" mb-8">
+      <div className="mb-8">
         <a
           href="https://github.com/Osiris8"
           className="mr-4 hover-link"
@@ -19,7 +19,7 @@ function App() {
         </a>
       </div>
       {/* Votre nom au centre */}
-      <h1 className="text-6xl font-bold mb-4">Osiris Migan</h1>
+      <h1 className="text-4xl font-bold mb-4 ">Osiris Migan</h1>
       {/* Paragraphe de description */}
       <p className="text-lg max-w-xl text-center">
         Enthusiastic web developer, specializing in React, Node.js, and MongoDB.
