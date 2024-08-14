26f0d93d8f70002878c5da45c0ab11ed48a50919
Osiris8 <miganosiris8@gmaaail.com>
2024-08-14 16:30:14 +0200

    Update work

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -60,24 +60,24 @@ export const DATA = {
 
   work: [
     {
-      company: "Atomic Finance",
-      href: "https://atomic.finance",
+      company: "OpenClassrooms",
+      href: "https://openclassrooms.com/en/",
       badges: [],
       location: "Remote",
-      title: "Bitcoin Protocol Engineer",
-      logoUrl: "/atomic.png",
-      start: "May 2021",
-      end: "Oct 2022",
+      title: "Web Developer Mentor",
+      logoUrl: "/OC.png",
+      start: "Feb 2021",
+      end: "Today",
       description:
         "Implemented the Bitcoin discreet log contract (DLC) protocol specifications as an open source Typescript SDK. Dockerized all microservices and setup production kubernetes cluster. Architected a data lake using AWS S3 and Athena for historical backtesting of bitcoin trading strategies. Built a mobile app using react native and typescript.",
     },
     {
-      company: "Shopify",
+      company: "SeeMarket",
       badges: [],
-      href: "https://shopify.com",
+      href: "https://www.see-markets.com/",
       location: "Remote",
       title: "Software Engineer",
-      logoUrl: "/shopify.svg",
+      logoUrl: "seemarket.png",
       start: "January 2021",
       end: "April 2021",
       description:
