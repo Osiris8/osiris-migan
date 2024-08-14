99f4619c7ca6cccafa406e8737475a33c002b32f
Osiris8 <miganosiris8@gmaaail.com>
2024-08-14 22:21:34 +0200

    update project

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -112,10 +112,10 @@ export const DATA = {
     {
       title: "OpenTranscriptAI",
       href: "https://opentranscriptai.com/",
-      dates: "Jan 2024 - Feb 2024",
+      dates: "Jan 2024 - March 2024",
       active: true,
       description:
-        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+        "[OpentranscriptAI](https://www.opentranscriptai.com/) is an online platform that allows you to easily convert audio or video files into text. Our transcription service uses advanced technology to provide fast, accurate transcriptions, enabling you to make the most of your audiovisual content.",
       technologies: [
         "Next.js",
         "Typescript",
